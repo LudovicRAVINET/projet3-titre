b75bff2279cb1dbbc93576fc49709dcaa396bead
Ludovic RAVINET <ludovic.ravinet@gmail.com>
2021-05-31 21:15:23 +0200

    added diary message form

--- src/Controller/WeddingController.php
+++ src/Controller/WeddingController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\MessageType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use DateTime;
 
 /**
 * @Route("/wedding", name="wedding_")
@@ -20,11 +25,34 @@ class WeddingController extends AbstractController
         return $this->render('wedding/index.html.twig');
     }
 
-    /**
-     * @Route("/{id}/dairy", name="dairy")
+   /*/**
+     * @Route("/{id}/diary", name="diary")
      */
-    public function dairy(int $id): Response
+   /* public function diary(int $id): Response
     {
-        return $this->render('wedding/dairy.html.twig');
+        return $this->render('wedding/diary.html.twig');
+    }*/
+
+    /**
+     * @Route("/diary", name="diary", methods={"GET","POST"})
+     */
+    public function diary(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $message = new Message();
+        $message->setMessageDateTime(new DateTime('now'));
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('wedding_diary');
+        }
+
+        return $this->render('wedding/diary.html.twig', [
+            'message' => $message,
+            'form' => $form->createView()
+        ]);
     }
 }
