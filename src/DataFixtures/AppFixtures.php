f51ed8149c866d87d5ee1d5989d5fa7949d96d24
Lulu-Soso <lucas.sokha@gmail.com>
2021-06-07 12:06:56 +0200

    created files login and logout

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,35 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $user = new User();
+
+        $hash = $this->encoder->encodePassword($user, "password");
+
+        $user->setEmail("pierre@gmail.com")
+            ->setFullName("Pierre Martin")
+            ->setPassword($hash);
+
+        $manager->persist($user);
+
 
         $manager->flush();
     }
