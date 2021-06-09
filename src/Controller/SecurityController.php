5f09bb5f3641026338c51958dcf3b54435ee3307
Bachir-Ndiaye <amadou.n-diaye@hotmail.com>
2021-06-09 10:59:49 +0200

    create register form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,31 +2,77 @@
 
 namespace App\Controller;
 
-use App\Form\LoginType;
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class SecurityController extends AbstractController
 {
+
     /**
-     * @Route("/login", name="security_login")
+     * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $utils): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $form = $this->createForm(LoginType::class, ['email' => $utils->getLastUsername()]);
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
-            'formView' => $form->createView(),
-            'error' => $utils->getLastAuthenticationError()
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
     }
 
     /**
-     * @Route("/logout", name="security_logout")
+     * @Route("/logout", name="app_logout")
      */
-    public function logout(): void
+    public function logout(): Response
     {
+        return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(
+        Request $request,
+        EntityManagerInterface $manager,
+        UserPasswordEncoderInterface $encoder
+    ): Response {
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            //Automatic login after registration
+            $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+            $this->container->get('security.token_storage')->setToken($token);
+            $this->container->get('session')->set('_security_main', serialize($token));
+
+            return $this->render('confirm/index.html.twig');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
