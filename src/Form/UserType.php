06940f28cffb038184625d150a9ecf8c8a24c6c5
Ludovic RAVINET <ludovic.ravinet@gmail.com>
2021-07-19 17:35:37 +0200

    added date constraint on register form birthdate field

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
@@ -12,6 +13,13 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class UserType extends AbstractType
 {
+    private string $today;
+
+    public function __construct()
+    {
+        $this->today = (new DateTime('today'))->format('Y-m-d');
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -22,6 +30,9 @@ class UserType extends AbstractType
             ->add('birthDate', BirthdayType::class, [
                 'label' => 'Date de naissance',
                 'widget' => 'single_text',
+                'attr' => [
+                    'max' => $this->today
+                ]
             ]);
     }
 }
