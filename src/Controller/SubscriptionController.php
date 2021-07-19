bc0fe554216dbc00425d55feea57591616a3193f
Ludovic RAVINET <ludovic.ravinet@gmail.com>
2021-07-19 18:12:43 +0200

    fixed bug about register confirm subscription save in DB

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\SubscriptionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +16,21 @@ class SubscriptionController extends AbstractController
     /**
      * @Route("/choice/{id}", name="choice")
      */
-    public function choice(): Response
-    {
+    public function choice(
+        int $id,
+        EntityManagerInterface $manager,
+        SubscriptionRepository $subscripRepository
+    ): Response {
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
+
+        $subscriptionChoice = $subscripRepository->find($id);
+        if ($subscriptionChoice !== null) {
+            $subscriptionChoice->addUser($user);
+        }
+
+        $manager->flush();
+
         return $this->redirectToRoute('profile_index', ['id' => $user->getId()]);
     }
 }
